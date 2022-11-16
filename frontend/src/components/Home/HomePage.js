61c5035982d06e16c482326fca840be1ebbc48d6
Kachi-Ezekwem <onyekachi.ezekwem@mail.mcgill.ca>
2022-11-16 16:39:04 -0500

    Created HomePage and Assets folders, and created homepage with styling

---
+++ frontend/src/components/Home/HomePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import './HomePageStyles.css';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="content">
+                <h1>FIT GARAGE</h1>
+
+                <h2>RISE TO THE CHALLENGE</h2>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
