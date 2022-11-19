fd4d413a21c5289aa0a3507e5b2181f84a638644
Kachi-Ezekwem <onyekachi.ezekwem@mail.mcgill.ca>
2022-11-19 10:00:24 -0500

    Created routing from homePage to signup page using the button. And created routing to login Page

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,13 +1,24 @@
 import React from "react";
 import './NavbarStyles.css';
+import { useNavigate, Routes, Route } from "react-router-dom";
+import { Signup } from "../Signup/Signup.js";
+import { Login } from "../Login/Login.js";
 
 const Navbar=() => {
+    const navigate = useNavigate();
+
+    const navigateSignUp = () => {
+        // 👇️ navigate to /
+        navigate('/signup');
+      };
+
     return (
         <div className="navbar">
             <div className="container">
                 
                 <ul className="nav-menu">
-                    <button> SIGN UP/LOGIN</button>
+                    <button onClick={navigateSignUp}> SIGN UP/LOGIN</button>
+
                 </ul>
             </div>
         </div>
