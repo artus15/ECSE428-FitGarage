f31c5dd5382ac97d7207b97513d15cce838e5a82
Cesar Lahoud <39594631+Czarlhd@users.noreply.github.com>
2022-11-20 18:27:30 -0500

    Log in page (#69)
    * created front end of log in page
    
    * updated tab title and logo

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,5 +1,95 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./style.module.css";
+
 export const Login = () => {
-    return (
-    <div>Login Page</div>
-    )
-}
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const navigate = useNavigate();
+
+	const navigateSignUp = () => {
+		navigate("/signup");
+	};
+
+	const login = () => {
+		console.log("login");
+	};
+
+	return (
+		<div
+			style={{
+				background: "black",
+				height: "100%",
+				width: "100%",
+				position: "absolute",
+			}}
+		>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					flexDirection: "column",
+					padding: "10rem",
+				}}
+			>
+				<div>
+					<text style={{ color: "orange", fontSize: "5rem" }}>
+						Log In
+					</text>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						padding: "1rem",
+					}}
+				>
+					<input
+						className={styles.input}
+						type="text"
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<input
+						className={styles.input}
+						type="text"
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<div>
+					<button
+						onClick={login}
+						style={{
+							backgroundColor: "orange",
+							paddingLeft: "1rem",
+							paddingRight: "1rem",
+							paddingTop: "0.5rem",
+							paddingBottom: "0.5rem",
+							borderRadius: "0.5rem",
+						}}
+					>
+						Log in
+					</button>
+				</div>
+				<div>
+					<button
+						style={{
+							border: "none",
+							backgroundColor: "transparent",
+						}}
+						onClick={navigateSignUp}
+					>
+						<text style={{ color: "orange" }}>
+							Click here to sign up
+						</text>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,28 +1,26 @@
 import React from "react";
-import './NavbarStyles.css';
-import { useNavigate, Routes, Route } from "react-router-dom";
-import { Signup } from "../Signup/Signup.js";
-import { Login } from "../Login/Login.js";
+import "./NavbarStyles.css";
+import { useNavigate } from "react-router-dom";
+// import { Signup } from "../Signup/Signup.js";
+// import { Login } from "../Login/Login.js";
 
-const Navbar=() => {
-    const navigate = useNavigate();
+const Navbar = () => {
+	const navigate = useNavigate();
 
-    const navigateSignUp = () => {
-        // 👇️ navigate to /
-        navigate('/signup');
-      };
+	const navigateSignUp = () => {
+		// 👇️ navigate to /
+		navigate("/signup");
+	};
 
-    return (
-        <div className="navbar">
-            <div className="container">
-                
-                <ul className="nav-menu">
-                    <button onClick={navigateSignUp}> SIGN UP/LOGIN</button>
+	return (
+		<div className="navbar">
+			<div className="container">
+				<ul className="nav-menu">
+					<button onClick={navigateSignUp}> SIGN UP/LOGIN</button>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-                </ul>
-            </div>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
